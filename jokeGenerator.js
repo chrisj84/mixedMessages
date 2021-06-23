2c6255e5c80f3f30f19ef477ce9ae25a29730124
Chris <chris.m.joyce@hotmail.com>
2021-06-23 23:09:39 +0100

    Created one array

---
+++ jokeGenerator.js
@@ -0,0 +1,20 @@
+const joke1 = ['Did you hear about the mathematician who’s afraid of negative numbers? ==> He’ll stop at nothing to avoid them.',
+'Why do we tell actors to “break a leg?” ==> Because every play has a cast.',
+'Yesterday I saw a guy spill all his Scrabble letters on the road. I asked him, “What’s the word on the street?”',
+'Why don’t scientists trust atoms? Because they make up everything.',
+'What does Charles Dickens keep in his spice rack?  ==> The best of thymes, the worst of thymes.',
+'What do you call a fake noodle?  ==> An impasta.',
+'What do you call a pony with a cough? ==> A little horse.',
+'What do you call a magic dog? ==> A labracadabrador.',
+'What did the shark say when he ate the clownfish? ==> This tastes a little funny.',
+'Did you hear about the two people who stole a calendar? They each got six months.',
+'What breed of dog can jump higher than buildings? ==> Any dog, because buildings can’t jump.'
+];
+
+const joke2 = [''
+
+
+
+
+
+];
