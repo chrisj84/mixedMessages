ccfda425a08251603d3b18f6163ba476fd2e06cb
Chris <chris.m.joyce@hotmail.com>
2021-06-24 00:34:07 +0100

    Created arrays and random number generator and logged to console. Changed theme to DND

--- README.md
+++ README.md
@@ -11,16 +11,18 @@ Using Javascript design random messages. The messages should be split into 3 par
 
 ### Updates 
  * Have started another branch *olive1*to try to redo the arrays
+ * Have changed the subject of the messages from jokes to a dungeons and dragons theme. 
+ * Have tested using node and is sucessfull, due to this will merge *olive1* branch to the master branch. 
 
 #### To do list 
 
 * Create and matain a gitHub version control for this project. 
 * Maintain the REAME doc
 * Create a js file and create the code:
-    + Create 3 arrays containing strings for which can be put together in a random order. 
-    + Create a function which calls a radom index from each of the arrays. 
-    + concatenate the results from the arrays into one string 
-    + log the string to the console
+    + Create 3 arrays containing strings for which can be put together in a random order. :heavy_check_mark:
+    + Create a function which calls a radom index from each of the arrays :heavy_check_mark:
+    + concatenate the results from the arrays into one string :heavy_check_mark:
+    + log the string to the console :heavy_check_mark:
     + ***Extra*** - set up a simple HTML page with a button.
     + ***Extra*** link the js file to the page and get the js file to alter the HTML
 *  Test code on node.js and chrome if extra steps are required. 
---
+++ spellGenerator.js
@@ -0,0 +1,67 @@
+const spells  = ['FireBolt', 'Chromatic orb', 'Dragons Breath', 'FireBall', 'Acid Splash'];
+const damage = [8, 10, 12, 14, 16, 18, 20];
+const outcome = ['You have missed', 'You have hit', 'Your spell has been successful', 'You have hit and defeated the enemy.', ];
+
+//generates random numbers based on array length 
+const genRanNum = num =>{
+    let ranNum = Math.floor(Math.random() * num);
+    return ranNum;
+}
+//generates a spell
+const getSpell = () =>{
+    let randomNum = genRanNum(spells.length);
+    let chosenSpell = spells[randomNum];
+    console.log (chosenSpell);
+    return chosenSpell;
+
+}
+// genterates damage
+const getDamage = () =>{
+    const ranDamage = damage [genRanNum(damage.length)];
+    console.log (ranDamage);
+    return ranDamage;
+}
+//geterates a random outcome
+const getOutcome = () =>{
+
+    const ranOutcome = outcome [genRanNum(outcome.length)];
+    console.log (ranOutcome);
+    return ranOutcome
+}
+
+function creatMessage () {
+    let chosenSpell = getSpell();
+    let spellMod = 1;
+    switch (chosenSpell){
+        case 'FireBolt':
+            spellMod = 1.25;
+            break;
+
+        case 'Chromatic orb':
+            spellMod = 1.5;
+            break;
+        case 'Dragons Breath':
+            spellMod = 2;
+            break;
+        case 'FireBall':
+            spellMod = 3;
+            break; 
+        case 'Acid Splash':
+            spellMod = 1.25;
+            break;
+        default:
+            spellMod = 1;
+
+    }
+    let damageDone = getDamage()* spellMod;
+    let theOutCome = getOutcome();
+    if (theOutCome === 'You have missed'){
+        console.log (`${theOutCome}. The spell ${chosenSpell} did 0 damage to your enemy`)
+    }
+    else {
+        console.log (`${theOutCome}. The spell ${chosenSpell} did ${damageDone} damage.`)
+
+    }
+    
+}
+creatMessage();
